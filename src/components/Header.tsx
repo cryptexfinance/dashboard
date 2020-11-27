25b1c138163533c06eabcc7c2b1c9cf5bfc2ba3f
Cristian Espinoza <ceegarner@gmail.com>
2020-11-27 17:25:06 -0600

    copy address

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import Nav from "react-bootstrap/esm/Nav";
 import Button from "react-bootstrap/esm/Button";
+import OverlayTrigger from "react-bootstrap/esm/OverlayTrigger";
+import Tooltip from "react-bootstrap/esm/Tooltip";
 import "../styles/header.scss";
 import ethers from "ethers";
 import NumberFormat from "react-number-format";
@@ -10,13 +12,29 @@ import TokensContext from "../state/TokensContext";
 import { makeShortAddress } from "../utils/utils";
 import { ReactComponent as TcapIcon } from "../assets/images/tcap-coin.svg";
 
-// TODO: Update TCAP Balance on mint and burn
 const Header = () => {
   const web3Modal = useContext(Web3ModalContext);
   const signer = useContext(SignerContext);
   const tokens = useContext(TokensContext);
   const [address, setAddress] = useState("");
   const [tokenBalance, setTokenBalance] = useState("0.0");
+
+  const copyCodeToClipboard = (e: React.MouseEvent) => {
+    e.preventDefault();
+    // Create new element
+    const el = document.createElement("textarea");
+    // Set value (string to be copied)
+    el.value = address;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute("readonly", "");
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand("copy");
+    // Remove temporary element
+    document.body.removeChild(el);
+  };
 
   useEffect(() => {
     const loadAddress = async () => {
@@ -33,7 +51,7 @@ const Header = () => {
           const currentBalance = await tokens.tcapToken?.balanceOf(currentAddress);
           setTokenBalance(ethers.utils.formatEther(currentBalance));
         });
-        setAddress(makeShortAddress(currentAddress));
+        setAddress(currentAddress);
         const currentTcapBalance = await tokens.tcapToken.balanceOf(currentAddress);
         setTokenBalance(ethers.utils.formatEther(currentTcapBalance));
       }
@@ -58,7 +76,17 @@ const Header = () => {
               decimalScale={2}
             />
           </h5>
-          <h5>{address}</h5>
+          <h5>
+            <OverlayTrigger
+              key="bottom"
+              placement="bottom"
+              overlay={<Tooltip id="tooltip-bottom">Click to Copy</Tooltip>}
+            >
+              <a href="/" onClick={copyCodeToClipboard} className="address">
+                {makeShortAddress(address)}
+              </a>
+            </OverlayTrigger>
+          </h5>
         </>
       ) : (
         <Button
