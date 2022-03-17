62390fcfad08a123d73b47c8a5b3bd3638cd90b9
jdestephen <jdestephen07@gmail.com>
2022-03-17 15:38:17 -0600

    more reward infoo  added

--- src/components/Vault/Monitoring/index.tsx
+++ src/components/Vault/Monitoring/index.tsx
@@ -557,7 +557,7 @@ export const Monitoring = () => {
           setVaultList(vData);
           setVaultsTotals(totals);
           setLiqLoaded(true);
-        }, 500);
+        }, 560);
       });
     }
     // Set pagination data
