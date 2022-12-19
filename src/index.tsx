a962f32c326f3334b9e47267fbc54ba493941254
jdestephen <jdestephen07@gmail.com>
2022-12-19 12:39:28 -0600

    issue n index page

--- src/index.tsx
+++ src/index.tsx
@@ -2,53 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/app.scss";
-import '@rainbow-me/rainbowkit/styles.css';
-import { OKX } from './utils/OKX';
-import '@rainbow-me/rainbowkit/styles.css';
-import { RainbowKitProvider, connectorsForWallets } from '@rainbow-me/rainbowkit';
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider, connectorsForWallets } from "@rainbow-me/rainbowkit";
 import {
-    metaMaskWallet,
-    rainbowWallet,
-    argentWallet,
-    ledgerWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+  metaMaskWallet,
+  rainbowWallet,
+  argentWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 import App from "./App";
+import { OKX } from "./utils/OKX";
 
 const { chains, provider, webSocketProvider } = configureChains(
-    [
-        chain.mainnet,
-        chain.polygon,
-        chain.optimism,
-        chain.arbitrum,
-        ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [chain.goerli] : []),
-    ],
-    [
-        alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID as string}),
-        publicProvider(),
-    ]
+  [
+    chain.mainnet,
+    chain.polygon,
+    chain.optimism,
+    chain.arbitrum,
+    ...(process.env.REACT_APP_ENABLE_TESTNETS === "true" ? [chain.goerli] : []),
+  ],
+  [
+    alchemyProvider({
+      apiKey: process.env.REACT_APP_ALCHEMY_ID || "",
+    }),
+    publicProvider(),
+  ]
 );
 
 const connectors = connectorsForWallets([
-    {
-        groupName: 'Recommended',
-        wallets: [
-            metaMaskWallet({ chains }),
-
-        ],
-    },
+  {
+    groupName: "Recommended",
+    wallets: [metaMaskWallet({ chains })],
+  },
 ]);
 
 const wagmiClient = createClient({
-    autoConnect: true,
-    connectors,
-    provider,
-    webSocketProvider,
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
 });
-
-
 
 ReactDOM.render(
   <React.StrictMode>
